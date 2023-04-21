d3f94adf4daa46a66cd6cf1a17fa887217fe9374
Andrew Nowakowski <64051143+andrewmnowakowski@users.noreply.github.com>
2023-04-21 12:19:10 -0400

    initial commit

---
+++ src/main/java/performance/test/performancetest/PerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package performance.test.performancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceTestApplication.class, args);
+    }
+
+}
